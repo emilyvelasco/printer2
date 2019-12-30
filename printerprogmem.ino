e84814602048595547958e32691fcbadb273ded0
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2019-12-30 10:14:41 -0800

    Update printerprogmem.ino

--- printerprogmem.ino
+++ printerprogmem.ino
@@ -14,19 +14,42 @@ const PROGMEM byte startup_message[startup_num_lines][startup_charsPerLine] = {
 };
 
 const int charsPerLine = 80;   // this is the max # of chars per line
-const int num_lines = 11;
+const int num_lines = 34;
 const PROGMEM  byte message[num_lines][charsPerLine] = {
   "    ",   // blank line
-"|-----------|",
-"| I         |",
-"| AM A      |",
-"| RABBIT    |",
-"| WITH A    |",
-"| SIGN      |",
-"|-----------|",
-"(\\__/) ||",
-"(o^o) ||",
-"/ 　 つ"
+"              iWs                                 ,W[",
+"              W@@W.                              g@@[",
+"             i@@@@@s                           g@@@@W",
+"             @@@@@@@W.                       ,W@@@@@@",
+"            ]@@@@@@@@@W.   ,_______.       ,m@@@@@@@@i",
+"           ,@@@@@@@@@@@@W@@@@@@@@@@@@@@mm_g@@@@@@@@@@[",
+"           d@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+"          i@@@@@@@A*~~~~~VM@@@@@@@@@@Af~~~~V*@@@@@@@@@i",
+"          @@@@@A~          'M@@@@@@A`         'V@@@@@@b",
+"         d@@@*`              Y@@@@f              V@@@@@.",
+"        i@@A`                 M@@P                 V@@@b",
+"       ,@@A                   '@@                   !@@@.",
+"      W@P                     @[                    '@@W",
+"      d@@            ,         ]!                     ]@@b",
+"     g@@[          ,W@@s       ]       ,W@@s           @@@i",
+"    i@@@[          W@@@@i      ]       W@@@@i          @@@@i",
+"   i@@@@[          @@@@@[      ]       @@@@@[          @@@@@i",
+"  g@@@@@[          @@@@@!      @[      @@@@@[          @@@@@@i",
+" d@@@@@@@          !@@@P      iAW      !@@@A          ]@@@@@@@i",
+"W@@@@@@@@b          '~~      ,Z Yi      '~~          ,@@@@@@@@@",
+"'*@@@@@@@@s                  Z`  Y.                 ,W@@@@@@@@A",
+"  'M@@@*f**W.              ,Z     Vs               ,W*~~~M@@@f",
+"    'M@    'Vs.          ,z~       'N_           ,Z~     d@P",
+"   M@@@       ~\\-__  __z/` ,gmW@@mm_ '+e_.   __=/`      ,@@@@",
+"    'VMW                  g@@@@@@@@@W     ~~~          ,WAf",
+"       ~N.                @@@@@@@@@@@!                ,Z`",
+"         V.               !M@@@@@@@@f                gf-",
+"          'N.               '~***f~                ,Z`",
+"            Vc.                                  _Zf",
+"              ~e_                             ,gY~",
+"                'V=_          -@@D         ,gY~ '",
+"                    ~\\=__.           ,__z=~`",
+"                         '~~~*==Y*f~~~",
 };
 
 // parallel port pin# = arduino pin#
