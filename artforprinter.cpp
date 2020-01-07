5cacf88f757ada102ef60e1e34865826227a59e4
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2020-01-07 14:10:47 -0800

    Update artforprinter.cpp

--- artforprinter.cpp
+++ artforprinter.cpp
@@ -48,3 +48,65 @@
 "                    | $$$$$$$$| $$ \\  $$| $$ \\  $$|  $$$$$$/ /$$$$$$       ",
 "                    |,,,,,,,,/|,,/  \\,,/|,,/  \\,,/ \\,,,,,,/ |,,,,,,/      ",
            
+
+
+                    *67                                                         
+                 MMM   MMM                                                      
+              :MM        MM                                                     
+             MM   MMMM$   M,                                                    
+           ?M+  MM    MM  MM                                                    
+          MM  MM      ,M  MM                                                    
+         MM  MM        M  MM                                                    
+        MM :M   MMM*  ~M  MM                                                    
+       MM  M  MM   M  MM  M                                                     
+      OM  M  MM    M  M  ,M                                                     
+      M  M, MM    MM MM  M,                                                     
+     MM MM $M     M  M  ,M                                                      
+    ,M  M  M     M? MD  M,                                                      
+    MM MM OM    MD MM  MN                                                       
+    M  M  MM  ,M? MM  MM                                                        
+   IM  M  *MMM*  MM  MM                                                         
+   MM $M        MM  MM                                                          
+   MM  M      MM   MM                                                           
+   8M  MM   MM8   M8                                                            
+    M    MMM    MM MMMMMMMMMMMMMMM                                              
+    NM        MMMMO..............:MMMM$                                         
+     *MM$~DMMM........................?MM,                                      
+         MM=.............................MM                                     
+        MM.................................M                                    
+        M..................................MM                                   
+        M...........OMMMMMMMMMMN=..........OM                                   
+        MM....?MMMMNO,,,,,,,,,,,OMMMMM.....MM                                   
+        MMM~MMN,,,,,,,,,,,,,,,,,,,,,,,MMM:MMM                                   
+        M77MMO,,,,,,,,,,,,,,,,,,,,,,,,,MMM7$M                                   
+        M77IIMMMM,,,,,,,,,,,,,,,,,,OMMM8I77OM                                   
+        M77II??++8MMMMMMMMMMMMMMMMM?++?II77MM                                 
+        M'MMI??++=~~::,,     ,,::~==++?IIMM=M                                   
+        M  'MM$++=~~::,,     ,,::~==++MMM  =M                                   
+        M    'IMMMM8::,      ,,::MMMMM'    =M                                   
+        M          'MMMMMMMMMMMMO'         =M                                 
+        M ##                              # M                                   
+        M # '#                            # M                                   
+        M #   #  #,,                 ##   # M                                   
+        M ##, #  # '#           ##   # #  # M                                   
+        M #  #   #     #####   ,##   #  # # M                                   
+        M #   #  ##;,  #    #  #  #  #  '## M                                   
+        M #,  #  #  '' #####   #  #  #    # M                                   
+        M  '##   #     #  #,   ### # #     OM                                   
+        M        ##;,  #   #, #    #       =M                                   
+        M           '  #    # #            +M                                 
+        MN                                 MM                                   
+        MMM                               MMM                                   
+        M7NMM,                         ,MM7,M                                   
+        M77I7MMM,                  ,NMMMII7,M                                   
+        M77II??+7MMMMMMMMMMMMMMMMMMM=+?II77,M                                 
+        MMIII??++=~~::,,     ,,::~==++?II77M                                    
+         MM7I??++=~~::,,     ,,::~==++?IIMM                                     
+           MMM?++=~~::,,     ,,::~==++$NM+                                      
+              MMMM=:::,,     ,,:~~OMMMM                                         
+                  'MMMMMMMMMMMMMM'                                              
+                                                                              
+                                                                                
+                                                                                
+                                                                                
+                                                                  
